d25c3f5b5b689926d7c1582f82209bea670d8d43
Ankita Arya <ankitaarya1207@gmail.com>
2025-04-10 18:58:55 +0530

    script, link and image components

---
+++ video 95/website/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Image className="mx-auto" width={100} height={100} src="https://i.pinimg.com/736x/05/44/9c/05449c51e772fd5b70a63c33c2936b7f.jpg" alt="" />
+    </div>
+  );
+}
