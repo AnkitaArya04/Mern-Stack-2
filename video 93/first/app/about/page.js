450c571183a96f677e2e3cd2cc8d0e6369132e3c
Ankita Arya <ankitaarya1207@gmail.com>
2025-04-10 16:21:13 +0530

    intro to next

---
+++ video 93/first/app/about/page.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="text-xl">Problem solved by Next.js</h1>
+      <ul>
+        <li>Full stack solution</li>
+        <li>file based routing</li>
+        <li>additional feature like router from next navigation</li>
+        <li>optimized rendering</li>
+      </ul>
+
+    </div>
+  )
+}
+
+export default page
