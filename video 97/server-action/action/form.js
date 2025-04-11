6b3b32badf9d70dab17cc751849be49672fa5677
Ankita Arya <ankitaarya1207@gmail.com>
2025-04-11 18:01:47 +0530

    server action

---
+++ video 97/server-action/action/form.js
@@ -0,0 +1,8 @@
+"use server"
+import fs from "fs/promises"
+export const submitAction=async (e)=>{
+    
+    console.log(e.get("name"),(e.get("add")))
+    let a=await fs.writeFile("Ankita.txt",`Name is ${e.get("name")} and adderess  is ${e.get("add")}`)
+   
+  }
